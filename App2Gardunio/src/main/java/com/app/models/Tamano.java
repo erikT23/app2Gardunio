a727b03df91578f7a57b18938e227bcfd4bbbac5
erikT23 <84143122+erikT23@users.noreply.github.com>
2023-03-23 20:16:07 -0600

    first commit

---
+++ App2Gardunio/src/main/java/com/app/models/Tamano.java
@@ -0,0 +1,41 @@
+package com.app.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tamanos")
+public class Tamano {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 45)
+	private String nombre;
+
+	public Tamano() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
---
+++ App2Gardunio/src/main/java/com/example/demo/App2GardunioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App2GardunioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App2GardunioApplication.class, args);
+	}
+
+}
